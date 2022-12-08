85ba37f12782fd071b57af7e985dd7b5a8ac3b3e
Ibeenoch <94024099+Ibeenoch@users.noreply.github.com>
2022-12-09 00:41:18 +0100

    Create Loading.js

---
+++ src/component/SideThree/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CircularProgress } from '@material-ui/core'
+
+const Loading = () => {
+  return (
+    <div>
+        <div>Loading...</div>
+        <CircularProgress />
+    </div>
+  )
+}
+
+export default Loading
