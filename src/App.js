061d7f08c285bf30353504856d9dddf72bf2a1c8
Ibeenoch <94024099+Ibeenoch@users.noreply.github.com>
2023-01-11 00:09:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,78 +1,31 @@
 import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './component/Home'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import Login from './component/Login'
-import NavBar from './component/NavBar'
-import Register from './component/Register'
-import Comment from './component/SideOne/Comment'
-import Notify from './component/SideOne/Notify'
-import Chat from './component/SideThree/Chat'
-import Explore from './component/SideTwo/Explore'
-import { useDispatch } from 'react-redux'
-import Postpage from './component/SideTwo/Postpage'
-import CreateProfile from './component/CreateProfile'
-import axios from 'axios'
-import { setLogin } from './features/userSlice'
-import Profile from './component/SideTwo/Profile'
-import Mapfollower from './component/SideTwo/Mapfollower'
-import MapFollowing from './component/SideTwo/MapFollowing'
-import PrivateRoute from './component/SideTwo/PrivateRoute'
-import Loading from './component/SideThree/Loading'
-import ViewProfile from './component/SideTwo/ViewProfile'
-import ViewCover from './component/SideTwo/ViewCover'
-import View from './component/SideTwo/View'
+import CreateProject from './Home/CreateProject';
+import MapTodo from './Home/MapTodo';
+import Navbar from './Home/Navbar';
+import NewTodo from './Home/NewTodo';
+import SearchProject from './Home/SearchProject';
+
+
+import Start from './Home/Start';
+
 
 const App = () => {
 
-  const user =  JSON.parse(localStorage.getItem('user'))
-const dispatch = useDispatch()
-
-useEffect(() => {
- dispatch(setLogin(user))
-}, [])
-
-useEffect(() => {
-  if('user' in localStorage){
-    const login = JSON.parse(localStorage.getItem('user'))
-    axios.defaults.headers.common["authorization"] = `Bearer ${login.token}`
-  }
-}, [user]) 
-
-
+  
   return (
-    <Router basename='/'>
-       <div style={{width: '100vw'}}>
-         <NavBar/>
-             
-       </div>
-      
-       <Routes>
-         <Route path='/' element={<PrivateRoute><Home /></PrivateRoute>} />
-         <Route path='/login' element={<Login/>} />
-         <Route path='/register' element={<Register />} />
-         <Route path='/notify' element={<Notify />} /> 
-         <Route path='/comment/:id' element={ <Comment />} />
-         <Route path='/chat' element={<Chat />} />
-         <Route path='/profile' element={<PrivateRoute ><Profile /></PrivateRoute> } />
-         <Route path='/profile/:id' element={<PrivateRoute ><Profile /></PrivateRoute> } />
-         <Route path='/followers' element={<PrivateRoute ><Mapfollower /></PrivateRoute> } />
-         <Route path='/following' element={<PrivateRoute ><MapFollowing /></PrivateRoute> } />
-         <Route path='/explore' element={<PrivateRoute ><Explore /></PrivateRoute> } />
-         <Route path='/postpage/:id' element={<PrivateRoute ><Postpage /></PrivateRoute> } />
-         <Route path='/postpage' element={<PrivateRoute ><Postpage /></PrivateRoute> } />
-         <Route path='/loading' element={<Loading />   } />
-         <Route path='/view/:id' element={<View/>   } />
-         <Route path='/viewprofile/:id' element={<ViewProfile/>   } />
-         <Route path='/viewcover/:id' element={<ViewCover/>   } />
-         <Route path='/createprofile' element={<CreateProfile />   } />
-         <Route path='/createprofile/:id' element={ <PrivateRoute ><CreateProfile /></PrivateRoute>  } />
-       </Routes>
-        <ToastContainer />
-    </Router>
-   
+   <Router>
+       <Navbar />
+    <Routes>
+    <Route path='/'  element={<Start />}/>
+    <Route path='/todo'  element={<NewTodo />}/>
+    <Route path='/todos'  element={<MapTodo />}/>
+    <Route path='/search'  element={<SearchProject />}/>
+    <Route path='/createproject'  element={<CreateProject />}/>
+
+    </Routes>
+   </Router> 
   )
 }
 
-export default App
+export default App;
