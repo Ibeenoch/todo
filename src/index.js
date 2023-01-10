7974c66175e944dbe2da25aaa4ae4deb5081deb4
Ibeenoch <94024099+Ibeenoch@users.noreply.github.com>
2023-01-11 00:10:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-import React, { useEffect } from "react";
+import React from 'react';
 import ReactDom from 'react-dom'
-import { Provider } from "react-redux";
-import { store } from "./features/store";
-import App from "./App";
-import { getPost } from "./features/blogSlice";
-import { getProfile } from "./features/api";
-import { alluser } from "./features/userSlice";
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistStore } from 'redux-persist'
+import App from './App';
+import {Provider} from 'react-redux'
+import store from './feature/store'
 
-
-export let persistor = persistStore(store)
-
-ReactDom.render(
-<Provider store={store}>
-    <PersistGate persistor={persistor}>
-     <App />    
-    </PersistGate>
-</Provider>
-, document.getElementById('root'))
+ ReactDom.render( 
+ <Provider store={store}>
+      <App/>   
+ </Provider>
+ , document.getElementById('root'))
